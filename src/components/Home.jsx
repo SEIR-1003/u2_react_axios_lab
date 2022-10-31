ab757be32c7132c97b7da5b59fac0e3164f172ff
Jack Viana <jack.viana2019@gmail.com>
2022-10-31 18:13:01 -0400

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
 
 return (
-<img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Star_Wars_Logo..png"></img>
+<img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Star_Wars_Logo..png"></img>
 
 )
 
