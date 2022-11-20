a9b46d45049739e098b684eccce11984ada43a81
Imdad Hossain <imdadhossain24@gmail.com>
2022-11-20 18:46:34 -0500

    Homework made up

---
+++ star-wars/src/App.js
@@ -0,0 +1,15 @@
+import StarshipsList from "./components/StarshipsList";
+import Planets from "./components/Planets";
+import Films from "./components/Films";
+import Main from "./components/Main";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
