cd6f9cf3fdbcf37c5d11059b79d21a8fdf50ad2c
Fernando <me@fdorado.com>
2023-02-21 20:34:01 -0500

    Added video to YouTube video to landing page.

--- star-wars-app/src/Landing.js
+++ star-wars-app/src/Landing.js
@@ -1,5 +1,8 @@
 export default function Landing() {
     return(
-        <h2>Landing page content goes here</h2>
+        <div className="video-container">
+            <iframe width="1022" height="575" src="https://www.youtube.com/embed/tGsKzZtRwxw" title="Star Wars Intro HD 1080p" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+
     )
 }
